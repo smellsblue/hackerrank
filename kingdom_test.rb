587763ac023fefeb99225d03378b4226b50f9f4f
Mike Virata-Stone <mike@virata-stone.com>
2017-01-29 22:09:01 -0800

    More cases working

--- kingdom_test.rb
+++ kingdom_test.rb
@@ -18,8 +18,18 @@ class KingdomTest < Minitest::Test
     assert_equal 2, kingdom.num_paths
   end
 
+  def test_two_simple_paths_through_a_known_city
+    kingdom = Kingdom.parse(["5 5", "1 2", "2 4", "2 3", "3 4", "4 5"])
+    assert_equal 2, kingdom.num_paths
+  end
+
   def test_road_past_end
     kingdom = Kingdom.parse(["3 2", "1 3", "3 2"])
     assert_equal 1, kingdom.num_paths
+  end
+
+  def test_simple_loop
+    kingdom = Kingdom.parse(["3 3", "1 2", "2 1", "2 3"])
+    assert_equal "INFINITE PATHS", kingdom.num_paths
   end
 end
--- path_finder.rb
+++ path_finder.rb
@@ -3,29 +3,47 @@ require "set"
 class PathFinder
   attr_reader :size
 
-  def initialize(from, to)
+  def initialize(from, to, check_infinite: true)
+    @check_infinite = check_infinite
     @found = Set.new
     @from = from
     @to = to
   end
 
+  def infinite?
+    @infinite
+  end
+
   def run
     @size = 0
-    follow_roads(@from)
+    @infinite = false
+    follow_roads(@from, [])
+    @size = "INFINITE PATHS" if infinite?
     self
   end
 
   private
 
-  def follow_roads(city)
-    @found << city
+  def follow_roads(city, path)
+    return if infinite?
+    path << city
 
-    if city == @to
+    if city == @to && path.size > 1
       @size += 1
+
+      if @check_infinite
+        @infinite = true if path.any? { |x| PathFinder.new(x, x, check_infinite: false).run.size != 0 }
+      end
     end
 
+    if @found.include?(city)
+      return
+    end
+
+    @found << city
+
     city.each_road do |road|
-      follow_roads(road.to)
+      follow_roads(road.to, path.dup)
     end
   end
 end
